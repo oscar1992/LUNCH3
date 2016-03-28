6dd70b77a257d093b1f55e2debea94c6f55d2723
oscar1992 <xirious@hotmail.com>
2016-03-28 15:35:09 -0500

    Initial Commit

---
+++ ConsumoSOAP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ConsumoSOAP
+//
+//  Created by Oscar Ramirez on 28/03/16.
+//  Copyright © 2016 Edumedio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
